33a97c8c3386a03cca3695ff2285a06737e00c32
yahia-M-nabil <yahiamnabil.asu@gmail.com>
2025-12-13 16:50:19 +0200

    Discrete_task1_project

---
+++ ROLI.hpp
@@ -0,0 +1,10 @@
+/*
+======================================
+AUTHOR:Yahia Mohamed Nabil ID:2300644
+DESCRIPTION:library made for logic rules and rules of inference
+*/
+#
+using namespace std;
+AND(){
+
+}
